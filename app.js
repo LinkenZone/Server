aae4fc4ac3056815d55f2f7dba36d85e22291f8b
NH1Tkendo <ngobatai1711@gmail.com>
2025-10-20 21:48:32 +0700

    Thêm PATCH vào chính sách CORS
    Signed-off-by: NH1Tkendo <ngobatai1711@gmail.com>

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
